8a411d3e64354991680b20852590274a4bc0d292
Ariadna <ariadnabertolo@gmail.com>
2020-07-02 13:04:47 +0200

    redux v.1

--- store.js
+++ store.js
@@ -1,2 +1,24 @@
-const store=[];
-export {store};
+import { createStore } from "redux";
+
+const initialState = { todos: [] };
+const store = createStore(todosReducer);
+
+function todosReducer(state = initialState.todos, action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      console.log("gatito");
+      return [...state, { title: action.title }];
+    default:
+      state;
+  }
+}
+
+function addTodo(title) {
+  return {
+    type: "ADD_TODO",
+    title: title,
+  };
+}
+
+store.dispatch(addTodo("hola caracola"));
+console.log(store.getState());
